4eae88aeaccf54074d1d907cde97a44dbb145a1d
cpt.kokao <cpt.kokao@gmail.com>
2024-01-12 11:49:29 +0300

    12.2. Разбор DI и IOC

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,18 @@ import { Server } from "http";
 import { UserController } from "./users/users.controller";
 import { LoggerService } from "./logger/logger.service";
 import { ExceptionFilter } from "./errors/exception.filter";
+import { ILogger } from "./logger/logger.interface";
 
 export class App {
   app: Express;
   server: Server;
   port: number;
-  logger: LoggerService;
+  logger: ILogger;
   userController: UserController;
   exceptionFilter: ExceptionFilter;
 
   constructor(
-    logger: LoggerService,
+    logger: ILogger,
     userController: UserController,
     exceptionFilter: ExceptionFilter
   ) {
