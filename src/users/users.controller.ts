998a1f9b012a2ee07d323fa3ce1f8244832a584f
cpt.kokao <cpt.kokao@gmail.com>
2024-02-12 12:26:18 +0300

    16.1. Создание JWT токена

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,12 +9,15 @@ import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { UserService } from './user.service';
 import { IUserController } from './users.controller.interface';
+import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../config/config.service.interface';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.ILogger) private loggerService: LoggerService,
 		@inject(TYPES.UserService) private userService: UserService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(loggerService);
 
@@ -44,7 +47,9 @@ export class UserController extends BaseController implements IUserController {
 		if (!result) {
 			return next(new HTTPError(401, 'Ошибка авторизации', 'login'));
 		}
-		this.ok(res, 'login ok');
+		const jwt = await this.signJWT(req.body.email, this.configService.get('SECRET'));
+
+		this.ok(res, { jwt });
 	}
 
 	async register(
@@ -57,5 +62,23 @@ export class UserController extends BaseController implements IUserController {
 			return next(new HTTPError(422, 'Такой пользователь уже существует'));
 		}
 		this.ok(res, { email: result.email, id: result.id });
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{ email, iat: Math.floor(Date.now() / 1000) },
+				secret,
+				{
+					algorithm: 'HS256',
+				},
+				(err, token) => {
+					if (err) {
+						reject(err);
+					}
+					resolve(token as string);
+				},
+			);
+		});
 	}
 }
